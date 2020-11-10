c91fa9540cd352d4bfe58b53b72159f6de8ab4da
ChenliTemple <Chenli_Temple@163.com>
2020-11-10 23:30:17 +0800

    创建render pipeline asset and render pipeline and camera render loop

---
+++ Assets/CustomRP/Runtime/CameraRender.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRender 
+{
+    ScriptableRenderContext context;
+    Camera camera;
+    
+    public void Render(ScriptableRenderContext context,Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        DrawVisbleGeometry();
+        Submit();
+    }
+    
+    public void DrawVisbleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    public void Submit()
+    {
+        context.Submit();
+    }
+}
---
+++ Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+
+    CameraRender cameraRender = new CameraRender();
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach(Camera camera in cameras)
+        {
+            cameraRender.Render(context, camera);
+        } 
+    }
+}
