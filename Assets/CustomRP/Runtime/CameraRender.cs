7946d2ab16244e2cadddfa26e2588a90e14394f3
ChenliTemple <Chenli_Temple@163.com>
2020-11-12 15:30:02 +0800

    custom render pipeline

--- Assets/CustomRP/Runtime/CameraRender.cs
+++ Assets/CustomRP/Runtime/CameraRender.cs
@@ -1,27 +1,81 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 
-public class CameraRender 
+public partial class CameraRender 
 {
     ScriptableRenderContext context;
     Camera camera;
+    CullingResults cullResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
     
     public void Render(ScriptableRenderContext context,Camera camera)
     {
         this.context = context;
         this.camera = camera;
 
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+            return;
+
+        SetUp();
         DrawVisbleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
+    }
+
+    public bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)){
+            cullResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
     
     public void DrawVisbleGeometry()
     {
+        SortingSettings sortingSettings = new SortingSettings(camera) {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        DrawingSettings drawSetting = new DrawingSettings(unlitShaderTagId,sortingSettings);
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullResults, ref drawSetting, ref filteringSettings);
         context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawSetting.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullResults, ref drawSetting, ref filteringSettings);
+    }
+
+    public void SetUp()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags cFlags = camera.clearFlags;
+        buffer.ClearRenderTarget(cFlags <= CameraClearFlags.Depth, cFlags == CameraClearFlags.Color, cFlags == CameraClearFlags.SolidColor ? camera.backgroundColor.linear : Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
     }
 
     public void Submit()
     {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
         context.Submit();
+    }
+
+
+    public void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
     }
 }
